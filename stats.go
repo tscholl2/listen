26fbbeb4a9ca630f7751f47c177d9eb0f919b857
Travis Scholl <tscholl2@uw.edu>
2016-01-04 22:00:32 -0800

    initial stats functions

---
+++ stats.go
@@ -0,0 +1,14 @@
+package main
+
+import "math"
+
+// returns mean, standard deviation as floats
+func stats(b []uint8) (float64, float64) {
+	n := len(b)
+	var sum, sumSq float64
+	for _, x := range b {
+		sum += float64(x)
+		sumSq += float64(x) * float64(x)
+	}
+	return sum / float64(len(b)), math.Sqrt(sumSq - sum*sum/float64(n)/float64(n-1))
+}
